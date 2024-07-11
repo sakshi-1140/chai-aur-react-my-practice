11f940fe32fae874e418c85dbe589517184d0717
Sakshi <sakshichoudhary000@gmail.com>
2024-07-11 15:02:30 +0530

    vedio3 completed'

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function chai(){
+    return(
+        <h3>from chai in react</h3>
+    )
+}
+
+export default chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function chai(){
+    return(
+        <h3>from chai in vite</h3>
+    )
+}
+
+export default chai
